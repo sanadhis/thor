981d2d7e8ab87dfb04b0de6622e740b56642938c
sanadhis <sanadhis@gmail.com>
2019-01-25 10:39:18 +0100

    [Feature-conda]: Add miniconda (minimum) and set anaconda 3 as default

--- install-miniconda.sh
+++ install-miniconda.sh
@@ -12,17 +12,17 @@ function print-banner () {
 function pre-installation () {
     print-banner "Getting anaconda installation script"
     mkdir -p $HOME/.temp
-    # With Python 2 (Anaconda 2)
-    # wget https://repo.continuum.io/archive/Anaconda2-5.0.1-Linux-x86_64.sh \
+    # With Python 2 (Miniconda 2)
+    # wget https://repo.anaconda.com/miniconda/Miniconda2-latest-Linux-x86_64.sh \
     #     -O $HOME/.temp/conda.sh
-    # With Python 3 (Anaconda 3) - default
-    wget https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh \
+    # With Python 3 (Miniconda 3) - default
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
         -O $HOME/.temp/conda.sh
     chmod +x $HOME/.temp/conda.sh
 }
 
 function main () {
-    print-banner "Installing anaconda"
+    print-banner "Installing miniconda"
     pushd $HOME/.temp
     ./conda.sh
     popd
